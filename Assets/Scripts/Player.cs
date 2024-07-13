2470bd9c5b24bac60ef0deac436438d2c4ab9c96
Dhimaan prasad <89119442+NotArchiveData@users.noreply.github.com>
2024-07-13 19:03:24 +0530

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    [SerializeField] private float moveSpeed = 11f;
+
+    private void Update() {
+
+        Vector2 input = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W)) {
+            input.y = +1;
+        } 
+        
+        if (Input.GetKey(KeyCode.A)) {
+            input.x = -1;
+        } 
+
+        if (Input.GetKey(KeyCode.S)) {
+            input.y = -1;
+        } 
+
+        if (Input.GetKey(KeyCode.D)) {
+            input.x = +1;
+        } 
+
+        input = input.normalized;
+
+        Vector3 movementDirection = new Vector3(input.x, 0f, input.y);
+        transform.position += moveSpeed * movementDirection * Time.deltaTime;
+
+        float rotationSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, movementDirection, Time.deltaTime * rotationSpeed);
+        {
+            
+        }
+    }
+}
