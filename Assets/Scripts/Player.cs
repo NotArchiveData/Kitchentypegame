6835a08c7cdb68dfd1d8be70a2e672b734e252b2
Dhimaan prasad <89119442+NotArchiveData@users.noreply.github.com>
2024-07-14 00:29:36 +0530

    collision behaviour

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
@@ -12,12 +14,51 @@ public class Player : MonoBehaviour {
     private void Update() {
         Vector2 input = gameInput.GetMovementVectorNormalized();
 
-        Vector3 movementDirection = new Vector3(input.x, 0f, input.y);
-        transform.position += moveSpeed * movementDirection * Time.deltaTime;
+        Vector3 moveDirection = new Vector3(input.x, 0f, input.y);
+        
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
 
-        walk = movementDirection != Vector3.zero;
+        if (!canMove) {
+            // Cannot move ur stuck L
+
+            // Move to the X axis instead
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove) {
+                // moving X axis
+                moveDirection = moveDirectionX;
+            }
+
+            else {
+                // cannot move X
+
+                // Now go ZZZZZ
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+
+                if (canMove) {
+                    // moving Z
+                    moveDirection = moveDirectionZ;
+                }
+
+                else {
+                    // ur actually stuck loser
+                }
+
+            }
+        }
+
+        if (canMove) {
+            transform.position += moveDirection * moveDistance;
+        }
+
+        walk = moveDirection != Vector3.zero;
         float rotationSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, movementDirection, Time.deltaTime * rotationSpeed);
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
 
     }
 
